e72fb5bdf59ffc19ad393e2e70e331a627f71850
Sebastian Matthew Koranda <sebasmk@ulrik.uio.no>
2023-03-30 13:52:39 +0200

    oppdaterer

--- app/src/main/java/com/team12/ElSpar/ui/ElSparViewModel.kt
+++ app/src/main/java/com/team12/ElSpar/ui/ElSparViewModel.kt
@@ -9,34 +9,61 @@ import androidx.lifecycle.viewmodel.viewModelFactory
 import com.team12.ElSpar.ElSparApplication
 import com.team12.ElSpar.domain.GetPowerPriceUseCase
 import com.team12.ElSpar.domain.GetProjectedPowerPriceUseCase
+import com.team12.ElSpar.model.PriceArea
+import kotlinx.coroutines.Dispatchers
+import com.team12.ElSpar.model.PricePeriod
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import java.io.IOException
+import java.time.LocalDateTime
 
 class ElSparViewModel(
     private val getPowerPriceUseCase: GetPowerPriceUseCase,
     private val getProjectedPowerPriceUseCase: GetProjectedPowerPriceUseCase,
+    initialPricePeriod: PricePeriod = PricePeriod.DAY,
+    initialPriceArea: PriceArea = PriceArea.NO1
 ) : ViewModel() {
     private val _uiState: MutableStateFlow<ElSparUiState> =
         MutableStateFlow(ElSparUiState.Loading)
     val uiState: StateFlow<ElSparUiState> = _uiState.asStateFlow()
 
+    private var currentPricePeriod = initialPricePeriod
+    private var currentPriceArea = initialPriceArea
+
     init {
         getPowerPrice()
     }
 
-    fun getPowerPrice() {
-        viewModelScope.launch {
+
+    fun getPowerPrice(
+        pricePeriod: PricePeriod = currentPricePeriod,
+        priceArea: PriceArea = currentPriceArea
+    ) {
+        viewModelScope.launch(Dispatchers.IO){
             _uiState.value = try {
                 ElSparUiState.Success(
-                    priceList = getPowerPriceUseCase()
+                    priceList = getPowerPriceUseCase(
+                        startTime = LocalDateTime.now().minusDays(pricePeriod.days.toLong()),
+                        priceArea = priceArea
+                    )
                 )
             } catch (e: IOException) {
                 ElSparUiState.Error
             }
         }
+    }
+
+    fun updatePricePeriod(pricePeriod: PricePeriod) {
+        currentPricePeriod = pricePeriod
+        getPowerPrice()
+    }
+
+    fun updatePriceArea(priceArea: PriceArea) {
+        currentPriceArea = priceArea
+        getPowerPrice()
     }
 
     companion object {
