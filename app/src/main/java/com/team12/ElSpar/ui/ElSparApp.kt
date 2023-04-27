c8c4230b6396aad42ab277ffe5629642f7b7efbd
iver barlinn korvald <iverbk@uio.no>
2023-04-27 12:07:10 +0200

    Added "loading animation" composable

--- app/src/main/java/com/team12/ElSpar/ui/ElSparApp.kt
+++ app/src/main/java/com/team12/ElSpar/ui/ElSparApp.kt
@@ -1,21 +1,31 @@
 package com.team12.ElSpar.ui
 
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.List
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import kotlinx.coroutines.delay
 
 @Composable
 fun NavBar(navController: NavHostController){
@@ -143,7 +153,8 @@ fun LoadingScreen(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        Text("LOADING")
+        //Text("LOADING")
+        LoadingAnimation()
     }
 }
 
@@ -158,4 +169,78 @@ fun ErrorScreen(
     ) {
         Text("ERROR")
     }
+}
+
+@Composable
+fun LoadingAnimation(
+    circleColor: Color = MaterialTheme.colorScheme.primaryContainer,
+    circleSize: Dp = 36.dp,
+    animationDelay: Int = 400,
+    initialAlpha: Float = 0.3f
+) {
+
+    // 3 circles
+    val circles = listOf(
+        remember {
+            Animatable(initialValue = initialAlpha)
+        },
+        remember {
+            Animatable(initialValue = initialAlpha)
+        },
+        remember {
+            Animatable(initialValue = initialAlpha)
+        }
+    )
+
+    circles.forEachIndexed { index, animatable ->
+
+        LaunchedEffect(Unit) {
+
+            // Use coroutine delay to sync animations
+            delay(timeMillis = (animationDelay / circles.size).toLong() * index)
+
+            animatable.animateTo(
+                targetValue = 1f,
+                animationSpec = infiniteRepeatable(
+                    animation = tween(
+                        durationMillis = animationDelay
+                    ),
+                    repeatMode = RepeatMode.Reverse
+                )
+            )
+        }
+    }
+
+    // container for circles
+    Row(
+        modifier = Modifier
+        //.border(width = 2.dp, color = Color.Magenta)
+    ) {
+
+        // adding each circle
+        circles.forEachIndexed { index, animatable ->
+
+            // gap between the circles
+            if (index != 0) {
+                Spacer(modifier = Modifier.width(width = 6.dp))
+            }
+
+            Box(
+                modifier = Modifier
+                    .size(size = circleSize)
+                    .clip(shape = CircleShape)
+                    .background(
+                        color = circleColor
+                            .copy(alpha = animatable.value)
+                    )
+            ) {
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun AppPreview() {
+    LoadingAnimation()
 }
